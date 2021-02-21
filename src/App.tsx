c6b3ce38b6877ce2981938a1fc494410197c7417
goohooh <piriri11@gmail.com>
2021-02-21 14:41:31 +0900

    init todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+type Todo = Readonly<{
+  id: number
+  text: string
+  done: boolean
+}>
+
+type Todos = readonly Todo[]
+
+const initialTodo: Todos = [
+  {
+    id: 1,
+    text: 'hello',
+    done: false,
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState<Todos>(initialTodo);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          todos.map(todo => (
+            <li>
+              <input type="checkbox" checked={todo.done} />
+              {todo.text}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
