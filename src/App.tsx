efb2c18ac2de3497154b04db06ab72842e07a6f1
goohooh <piriri11@gmail.com>
2021-02-21 15:24:42 +0900

    implement inputTodo, addTodo, todo list, completeAll

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ type Todo = Readonly<{
 
 type Todos = readonly Todo[]
 
-const initialTodo: Todos = [
+type CompletedTodo = Todo & { readonly done: true }
+
+const initialTodos: Todos = [
   {
     id: 1,
     text: 'hello',
@@ -18,19 +20,64 @@ const initialTodo: Todos = [
 ];
 
 function App() {
-  const [todos, setTodos] = useState<Todos>(initialTodo);
+  const [inputTodo, setInputTodo] = useState('');
+  const [todos, setTodos] = useState<Todos>(initialTodos);
+
+  function addTodo(e: React.ChangeEvent<HTMLInputElement>): void {
+    setInputTodo(e.target.value)
+  }
+
+  function onEnter(e: React.KeyboardEvent<HTMLInputElement>): void {
+    if (e.key === 'Enter') {
+      setTodos(state => {
+        return [
+          ...state,
+          {
+            id: todos.length,
+            text: inputTodo,
+            done: false,
+          }
+        ]
+      });
+
+      setInputTodo('');
+    }
+  }
+
+  function onCheck(todo: Todo, index: number): void {
+    const newTodo = { ...todo, done: !todo.done };
+    const newTodos = [...todos];
+    
+    newTodos.splice(index, 1, newTodo);
+
+    setTodos(newTodos);
+  }
+
+  function completeAll() {
+    const completedTodos: CompletedTodo[] = todos.map(todo => ({
+      ...todo,
+      done: true,
+    }));
+
+    setTodos(completedTodos);
+  }
+
   return (
     <div className="App">
+      <input value={inputTodo} onChange={addTodo} onKeyPress={onEnter} />
+
       <ul>
         {
-          todos.map(todo => (
+          todos.map((todo, index) => (
             <li>
-              <input type="checkbox" checked={todo.done} />
+              <input type="checkbox" checked={todo.done} onChange={() => onCheck(todo, index)} />
               {todo.text}
             </li>
           ))
         }
       </ul>
+
+      <button onClick={completeAll}>Complete All</button>
     </div>
   );
 }
